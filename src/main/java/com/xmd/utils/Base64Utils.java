ced213ba3784c9a8932ce128ee8ad68e8be3de7b
i21mscz23 <i21mscz23@163.com>
2022-04-11 18:16:09 +0800

    feat Base64Utils

---
+++ src/main/java/com/xmd/utils/Base64Utils.java
@@ -0,0 +1,21 @@
+package com.xmd.utils;
+
+import java.util.Base64;
+
+public class Base64Utils {
+
+
+    /**
+     * Base64 获取图片字符串
+     * @param bytes
+     * @return
+     */
+    public static String getImgStr(byte[] bytes){
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
+    public static byte[] getByte(String imgStr){
+        return Base64.getDecoder().decode(imgStr);
+    }
+
+}
